fcd3b05b3d19931d1bb7b50aa7bc1172942d422e
Brian aguiar <briantbini@outlook.com>
2021-04-30 14:25:55 -0300

    Class man com os metodos criados sendo ele addItems removeItems

--- FixacaoOrder/Entities/Order.cs
+++ FixacaoOrder/Entities/Order.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using FixacaoOrder.Entities.Enums;
+using FixacaoOrder.Entities;
 namespace FixacaoOrder.Entities
 {
     class Order
@@ -10,10 +11,19 @@ namespace FixacaoOrder.Entities
 
         public OrderStatus Status { get; set; }
 
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
         public Order()
         {
 
         }
-
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
     }
 }
