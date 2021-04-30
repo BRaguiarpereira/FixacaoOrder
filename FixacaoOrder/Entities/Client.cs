3b63e6e799037fe2ca429e73b9b5feae91830bba
Brian aguiar <briantbini@outlook.com>
2021-04-30 14:43:46 -0300

    Escopo criado com todas funcoes das classes e sem o main criado

--- FixacaoOrder/Entities/Client.cs
+++ FixacaoOrder/Entities/Client.cs
@@ -15,5 +15,20 @@ namespace FixacaoOrder.Entities
         {
 
         }
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", ("
+                + BirthDate.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
     }
 }
--- FixacaoOrder/Entities/Order.cs
+++ FixacaoOrder/Entities/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using FixacaoOrder.Entities.Enums;
 using FixacaoOrder.Entities;
+using System.Globalization;
 namespace FixacaoOrder.Entities
 {
     class Order
@@ -10,12 +11,22 @@ namespace FixacaoOrder.Entities
         public DateTime Moment { get; set; }
 
         public OrderStatus Status { get; set; }
-
+        //Lembra que tem que criar os tipo quando são referenciado
+        //exemplo Order esta relacionado com o Client
+        public Client Client { get; set; }
+        // E caso for uma composiçao cria um tipo list relacionado com a classe em especifico
+        // exemplo Order com OrderItem
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
         public Order()
         {
 
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
         }
         public void AddItem(OrderItem item)
         {
@@ -24,6 +35,30 @@ namespace FixacaoOrder.Entities
         public void RemoveItem(OrderItem item)
         {
             Items.Remove(item);
+        }
+        // Metodo total que pega o subtotal dos OrderItem 
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
         }
     }
 }
